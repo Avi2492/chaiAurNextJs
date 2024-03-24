fb7ef0d54d6e39c2d211b7d58bda955f1061bf11
Avinash Pandey <avinash.12009084@lpu.in>
2024-03-24 17:24:47 +0530

    Signup Done

--- src/helper/mailer.ts
+++ src/helper/mailer.ts
@@ -1,20 +1,29 @@
 import User from "@/models/userModels";
 import nodemailer from "nodemailer";
 import bcryptjs from "bcryptjs";
+
 export const sendEmail = async ({ email, emailType, userId }: any) => {
   try {
     const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
     if (emailType === "VERIFY") {
-      await User.findByIdAndUpdate(userId, { verifyToken });
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
     }
 
-    const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false,
+    const transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
       auth: {
-        user: "maddison53@ethereal.email",
-        pass: "jn7jnAPss4f63QBp6D",
+        user: String(process.env.USER),
+        pass: String(process.env.PASSWORD),
       },
     });
 
@@ -23,10 +32,16 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
       to: email,
       subject:
         emailType === "VERIFY" ? "VERIFY YOUR EMAIL" : "Reset your password",
-      html: "<b>Hello world?</b>", // html body
+      html: `<p>Click <a href="${
+        process.env.DOMAIN
+      }/verifyemail?token=${hashedToken}">here</a> to ${
+        emailType === "VERIFY" ? "Verify your email" : "reset your password"
+      } or copy paste the link below in your browser<br>${
+        process.env.DOMAIN
+      }/verifyemail?token=${hashedToken}</p>`,
     };
 
-    const mailResponse = await transporter.sendMail(mailOptions);
+    const mailResponse = await transport.sendMail(mailOptions);
     return mailResponse;
   } catch (error: any) {
     throw new Error(error.message);
